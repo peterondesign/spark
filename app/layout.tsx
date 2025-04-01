6b912fe5120b9f00db43c5f15c49d0905bb675c3
Your Name <you@example.com>
2025-04-01 10:50:41 +0100

    Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans } from "next/font/google"
 import { ThemeProvider } from '@/components/theme-provider';
-import CronitorScript from './components/CronitorScript';
+import Script from 'next/script';
 
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] })
 
@@ -78,13 +78,26 @@ export default function RootLayout({
         {/* These will be properly extended by each page's metadata.alternates */}
       </head>
       <body className={plusJakartaSans.className}>
+        {/* Microsoft Clarity analytics tracking script */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qx9ogw0g2g");
+            `
+          }}
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <CronitorScript />
           {children}
         </ThemeProvider>
       </body>
